39700f167d521eb2896eda3dc7ac1b0a6e0bb7f8
max.white <max.beliy2011@mail.ru>
2022-08-29 20:06:39 +0300

    Header funct fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,9 @@
 import { Greetings } from '../Greetings/Greetings';
 import HeaderCSS from './Header.module.css';
 
-export function Header({ name }) {
-  return (
-    <header className={HeaderCSS.header}>
-      My first react App
-      <Greetings name={name} />
-    </header>
-  );
-}
+export const Header = ({ name }) => (
+  <header className={HeaderCSS.header}>
+    My first react App
+    <Greetings name={name} />
+  </header>
+);
