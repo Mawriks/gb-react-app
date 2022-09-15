2038449dc3cd8dc0d895c6ec64684b733318e9d9
max.white <max.beliy2011@mail.ru>
2022-09-15 19:44:37 +0300

    lesson-6 fix

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,4 @@
 import { createStore } from 'redux';
 import { profileReducer } from './profile/reducer';
 
-export const composeEnhancers =
-  //@ts-ignore
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-export const store = createStore(profileReducer, composeEnhancers());
+export const store = createStore(profileReducer);
